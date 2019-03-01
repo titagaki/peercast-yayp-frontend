16da806f384fc2faf4ef9b3aff9336cdd4c66269
titagaki <megane.wo.gurun@gmail.com>
2019-03-01 19:57:31 +0900

    チャンネルリスト表示部分を作成

--- src/components/ChannelList.vue
+++ src/components/ChannelList.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="channel-list">
-    <vs-table search max-items="3" pagination :data="channels">
+  <div class="ch-list">
+    <vs-table
+            search
+            pagination
+            max-items="3"
+            :data="channels">
       <template slot="header">
         <h3>
           現在配信中のチャンネル
@@ -19,20 +23,20 @@
       </template>
 
       <template slot-scope="{data}">
-        <vs-tr :key="indextr" v-for="(tr, indextr) in data" >
-          <vs-td :data="data[indextr].Name">
-            <p>{{data[indextr].Name}}</p>
-            <p>[{{data[indextr].Genre}} - {{data[indextr].Description}}]「{{data[indextr].Comment}}」</p>
+        <vs-tr :data="tr" :key="i" v-for="(tr, i) in data">
+          <vs-td>
+            {{ data[i].Name }}<br>
+            [{{ data[i].Genre }} - {{ data[i].Description }}]「{{ data[i].Comment }}」
           </vs-td>
 
-          <vs-td :data="data[indextr].Listeners">
-            <p>{{data[indextr].Listeners}} / {{data[indextr].Relays}}</p>
-            <p>{{data[indextr].Age}}</p>
+          <vs-td>
+            {{ data[i].Listeners }} / {{ data[i].Relays }}<br>
+            {{ formatTime(data[i].Age) }}
           </vs-td>
 
-          <vs-td :data="data[indextr].ContentType">
-            <p>{{data[indextr].ContentType}}</p>
-            <p>{{data[indextr].Bitrate}} kbps</p>
+          <vs-td>
+            {{ data[i].ContentType }}<br>
+            {{ data[i].Bitrate }} kbps
           </vs-td>
         </vs-tr>
       </template>
@@ -42,16 +46,39 @@
 
 <script>
   export default {
-    data:()=>({
-      channels:[{"ID":8,"GnuID":"1FAF45207E3CF1CB28838893A71B224B","Name":"kkt","Bitrate":1660,"ContentType":"FLV","Listeners":8,"Relays":5,"Age":70,"Genre":"game","Description":"Project Zomboid","Url":"http://jbbs.shitaraba.net/bbs/read.cgi/game/60476/1549254358/","Comment":"死ぬマン にんげんとのたたかい","TrackArtist":"","TrackTitle":"","TrackAlbum":"","TrackGenre":"","TrackContact":"","HiddenListeners":false,"TrackerIP":"121.115.0.152:7144","TrackerDirect":false,"IsPlaying":true},{"ID":9,"GnuID":"C19506B7717D474F7884E999638DE3DF","Name":"だいちゃ","Bitrate":1628,"ContentType":"FLV","Listeners":7,"Relays":5,"Age":205,"Genre":"Game","Description":"テトリス99","Url":"http://jbbs.shitaraba.net/bbs/read.cgi/game/37056/1531041366/","Comment":"まったり短時間","TrackArtist":"","TrackTitle":"","TrackAlbum":"","TrackGenre":"","TrackContact":"","HiddenListeners":false,"TrackerIP":"133.232.141.34:7155","TrackerDirect":false,"IsPlaying":true},{"ID":10,"GnuID":"D80DE8FA12561E649E95A6DE3BAE9C63","Name":"0501","Bitrate":1914,"ContentType":"FLV","Listeners":5,"Relays":14,"Age":216,"Genre":"","Description":"なんもしません","Url":"http://jbbs.shitaraba.net/music/29690/","Comment":"","TrackArtist":"","TrackTitle":"","TrackAlbum":"","TrackGenre":"","TrackContact":"","HiddenListeners":false,"TrackerIP":"210.255.248.80:7144","TrackerDirect":false,"IsPlaying":true},{"ID":11,"GnuID":"904EAAA0968D40F787C350EA7A7C4276","Name":"ひゅっくり","Bitrate":1628,"ContentType":"FLV","Listeners":12,"Relays":13,"Age":220,"Genre":"Game","Description":"Euro Truck Simulator 2","Url":"http://hyuku.net/test/read.cgi/bbs/1550257345/","Comment":"無免許なのに5拠点目開設を目指す！","TrackArtist":"","TrackTitle":"","TrackAlbum":"","TrackGenre":"","TrackContact":"","HiddenListeners":false,"TrackerIP":"60.112.35.218:7144","TrackerDirect":false,"IsPlaying":true}]
-    }),
+    data() {
+      return {
+        channels: []
+      }
+    },
+    mounted() {
+      const url = '/api/channels';
+      fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            return;
+          }
+          response.json().then(json => {
+            this.channels = json
+          });
+        })
+    },
+    methods: {
+      formatTime(t) {
+        const s = t % 60;
+        t = (t - s) / 60;
+        const m = t % 60;
+        const h = (t - m) / 60;
+        return h + ':' + (m < 10 ? '0' + m : m);
+      }
+    }
   }
 </script>
 
 <style lang="stylus" scoped>
-  .channel-list
-    width 70%
+  .ch-list
+    width: 70%
     margin: auto
   td
-    text-align left
+    text-align: left
 </style>
